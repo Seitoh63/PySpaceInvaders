79f487db8f2bc01a47f7233c6633076feb4ec142
Rafael Fajardo <rafael.a.fajardo@gmail.com>
2023-03-25 19:05:09 -0600

    found correct syntax for FULLSCREEN arg line 19

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class PySpaceInvaders:
     def __init__(self):
 
         # We create a surface in which sprites will be shown
-        self.window_surface = pygame.display.set_mode(WINDOW_SIZE, flags = FULLSCREEN)
+        self.window_surface = pygame.display.set_mode(WINDOW_SIZE, pygame.FULLSCREEN)
 
         # Variables for game loop
         self.update_time_delay = 0
